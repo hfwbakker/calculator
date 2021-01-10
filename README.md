38651e4ded6d5217f5cd2e57a7159069a954578b
Henri Bakker <hfwbakker@gmail.com>
2021-01-10 17:28:34 +0800

    basic functionality is there, except that it can't string values yet. see readme.md for POA

--- README.md
+++ README.md
@@ -5,5 +5,15 @@ Goal: make a completely functional calculator as per Odin Project guidelines fou
 Current status:
 At step 4, visuals have been created, event listeners have been added for all buttons (console.logs upon click), operate function has been written and works more or less.
 
+Calculator is functional up to point 5, meaning that all calculations (e.g. a + b = c) work, except that I can't string numbers yet. (point 6). There is some issue with floats turning to strings and strings turning to floats in the functions that are run.
+
+The plan: backup current script.js and refactor a new one: 
+    1) make a specific function applied to numbers (and decimal) only that updates to display as a string. 
+    2) Then make a function specifically for operators that takes the value from the display, parses it into a float ( parseFloat() ), and adds/subtracts/we
+    3) somehow need to have a way that automatically keeps current and last value, and moves them "down the line" every time a new value is added. (perhaps append values to an array that gets reset upon pressing C and adress them with arr[arr.length -1] and arr[arr.length -2]?) -> checked and it works (in "add" function) so yes lets make it a function that automatically adds it to a string.
+
+
+example: https://mrbuddh4.github.io/calculator/
+
 Next up:
 Make display reactive (e.g. click a number and it shows up in display). After that step 5 - 10.
--- script.js
+++ script.js
@@ -1,110 +1,183 @@
 //key functionality
-const keyReset = document.querySelector("#reset")
-keyReset.addEventListener("click", () => {
-    console.log("you clicked C")
-})
-
 const key0 = document.querySelector("#num0")
 key0.addEventListener("click", () => {
     console.log("you clicked 0")
+    displayValue += "0"
+    // dasArr = dasArr.concat(0)
+    displayUpdater()
 })
 
 const key1 = document.querySelector("#num1")
 key1.addEventListener("click", () => {
     console.log("you clicked 1")
+    displayValue += "1"
+    displayUpdater()
 })
 
 const key2 = document.querySelector("#num2")
 key2.addEventListener("click", () => {
     console.log("you clicked 2")
+    displayValue += "2"
+    displayUpdater()
 })
 
 const key3 = document.querySelector("#num3")
 key3.addEventListener("click", () => {
     console.log("you clicked 3")
+    displayValue += "3"
+    displayUpdater()
 })
 
 const key4 = document.querySelector("#num4")
 key4.addEventListener("click", () => {
     console.log("you clicked 4")
+    displayValue += "4"
+    displayUpdater()
 })
 
 const key5 = document.querySelector("#num5")
 key5.addEventListener("click", () => {
     console.log("you clicked 5")
+    displayValue += "5"
+    displayUpdater()
 })
 
 const key6 = document.querySelector("#num6")
 key6.addEventListener("click", () => {
     console.log("you clicked 6")
+    displayValue += "6"
+    displayUpdater()
 })
 
 const key7 = document.querySelector("#num7")
 key7.addEventListener("click", () => {
     console.log("you clicked 7")
+    displayValue += "7"
+    displayUpdater()
 })
 
 const key8 = document.querySelector("#num8")
 key8.addEventListener("click", () => {
     console.log("you clicked 8")
+    displayValue += "8"
+    displayUpdater()
 })
 
 const key9 = document.querySelector("#num9")
 key9.addEventListener("click", () => {
     console.log("you clicked 9")
-})
-
-const keyDivide = document.querySelector("#divide")
-keyDivide.addEventListener("click", () => {
-    console.log("you clicked divide")
-})
-
-const keySubtract = document.querySelector("#subtract")
-keySubtract.addEventListener("click", () => {
-    console.log("you clicked subtract")
-})
-
-const keyAdd = document.querySelector("#add")
-keyAdd.addEventListener("click", () => {
-    console.log("you clicked add")
-})
-
-const keyMultiply = document.querySelector("#multiply")
-keyMultiply.addEventListener("click", () => {
-    console.log("you clicked multiply")
-})
-
-const keyEquals = document.querySelector("#equals")
-keyEquals.addEventListener("click", () => {
-    console.log("you clicked equals")
+    displayValue += "9"
+    displayUpdater()
 })
 
 const keyDecimal = document.querySelector("#decimal")
 keyDecimal.addEventListener("click", () => {
     console.log("you clicked decimal")
+    displayValue += "."
+    displayUpdater()
 })
 
+/////////////////////
+///   OPERATORS   ///
+/////////////////////
+
+const keyAdd = document.querySelector("#add")
+keyAdd.addEventListener("click", () => {
+    console.log("you clicked add")
+    dasArr = dasArr.concat(parseFloat(displayValue))
+    operatorSymbol = "+"
+    updater()
+})
+
+const keyDivide = document.querySelector("#divide")
+keyDivide.addEventListener("click", () => {
+    console.log("you clicked divide")
+    operatorSymbol = "/"
+    updater()
+})
+
+const keySubtract = document.querySelector("#subtract")
+keySubtract.addEventListener("click", () => {
+    console.log("you clicked subtract")
+    operatorSymbol = "-"
+    updater()
+})
+
+const keyMultiply = document.querySelector("#multiply")
+keyMultiply.addEventListener("click", () => {
+    console.log("you clicked multiply")
+    operatorSymbol = "*"
+    updater()
+})
+
+const keyEquals = document.querySelector("#equals")
+keyEquals.addEventListener("click", () => {
+    console.log("you clicked equals")
+    displayValue = testFunc()
+    displayUpdater()
+})
+
+const keyReset = document.querySelector("#reset")
+keyReset.addEventListener("click", () => {
+    console.log("you clicked C")
+    displayValue = ""
+    lastDisplayValue = ""
+    dasArr = []
+    displayUpdater()
+})
+
+
 //display screen logic
-const display = document.querySelector(".display")
+const display = document.querySelector("#display")
+
+let displayValue = ""
+let lastDisplayValue = ""
+let runningValue = 0
+let operatorSymbol = ""
+let dasArr = []
+
+let displayNum = document.createElement("h3")
+
+const displayUpdater = function() {
+    // some if statement or function that rounds numbers to max 8 digits
+    displayNum.textContent = displayValue
+    display.appendChild(displayNum)
+}
+
+const updater = function() {
+    // lastDisplayValue = displayValue
+    lastDisplayValue = parseFloat(displayValue)
+    runningValue += parseFloat(displayValue) + lastDisplayValue
+    displayUpdater()
+    displayValue = ""
+}
 
 
 // calculation logic
-
-let operate = function(a, b, c) {
-    // a is user input int/float
-    // b is user input operator
-    // c is user input int/float
-
+let testFunc = function() {
+    let ans = 0
+    a = parseFloat(lastDisplayValue)
+    console.log(`lastDisplayValue (a) = ${lastDisplayValue}`)
+    b = operatorSymbol
+    console.log(`operatorSymbol (b) = ${operatorSymbol}`)
+    c = parseFloat(displayValue)
+    console.log(`displayValue (c) = ${displayValue}`)
     if (b == "/"){
-        return a / c
+        console.log(a / c)
+        ans = a / c
     }
     if (b == "-") {
-        return a - c
+        console.log(a - c)
+        ans = a - c
     }
     if (b == "+") {
-        return a + c
+        console.log(a + c)
+        ans = a + c
     }
     if (b == "*") {
-        return a * c
+        console.log(a * c)
+        ans = a * c
     }
+    return ans.toString()
 }
+
